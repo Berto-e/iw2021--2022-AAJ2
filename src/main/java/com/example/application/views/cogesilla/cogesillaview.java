92b57d8012b7960b712a68e2492f4f2912be79a4
JAcode9 <72512757+JAcode9@users.noreply.github.com>
2021-12-29 19:28:21 +0100

    Add files via upload
    avance en seleccion de silla, se diferencia entre clicada y no cliclada

--- src/main/java/com/example/application/views/cogesilla/cogesillaview.java
+++ src/main/java/com/example/application/views/cogesilla/cogesillaview.java
@@ -1,46 +1,85 @@
 package com.example.application.views.cogesilla;
 
+import com.example.application.classes.Pelicula;
 import com.example.application.views.MainLayout;
 import com.example.application.views.addpeli.addpeliview;
+import com.example.application.views.compra.compraview;
+import com.example.application.views.imagelist.ImageListView;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import java.util.Optional;
 
 @PageTitle("Escoger asiento")
 @Route(value = "asiento", layout = MainLayout.class)
 @Uses(Icon.class)
-public class cogesillaview extends VerticalLayout {
+
+
+public class cogesillaview extends VerticalLayout implements BeforeEnterObserver {
+    private int num_fila;
+    private int num_clicks = 0;
+    private int cont_asientos = 0;
+    private int num_elegida = 0;
+    private Button enviar = new Button("Seguir comprando");
     public cogesillaview() {
-        for (int i = 0; i < 10; i++) {
-            HorizontalLayout h = new HorizontalLayout();
-            for (int j = 0; j < 10; j++){
-                Image img = new Image("/images/silla-de-cine.png", "sillon");
-                img.setTitle(//guardamos i);
-                img.setText(//guardamos j);
-                img.setAlt(//numero de cliccada);
-                img.addClickListener(e -> {
-                    //img.setAlt(//numero de cliccada);
-                    //cont_total++
-                    //cont_asientos++
-                    //img.setSrc();
-                    String fotoantigua = img.getSrc();
-                    //compara las sources y hace lo que sea
-                    //if(compara y ya estaba seleccionado)
-                        //cambia al original y cont_asientos--
-                        //pasa por sesion poniendo la img.setatribute("img.getalt()",null)
-                    //por sesion se pasa la i y la j// la imagen// o probar con una clase como cardbox, num_asiento y num clicks
-                });
-                h.setSpacing(false);
-                h.setWidthFull();
-                h.setJustifyContentMode(JustifyContentMode.CENTER);
-                h.add(img);
-                add(h);
+        if (UI.getCurrent().getSession().getAttribute("filas") != null)
+            num_fila = (int) UI.getCurrent().getSession().getAttribute("filas");
+
+            for (int i = 0; i < num_fila; i++) {
+                HorizontalLayout h = new HorizontalLayout();
+                for (int j = 0; j < 10; j++) {
+                    Image img = new Image("/images/silla-de-cine.png", String.valueOf(num_elegida));
+                    img.setTitle(String.valueOf(i));
+                    img.setText(String.valueOf(j));
+                    img.addClickListener(e -> {
+                        num_elegida++;
+                        num_clicks++;
+                        cont_asientos++;
+                        img.setAlt(String.valueOf(num_elegida));
+                        String actual = img.getSrc();
+                        if (actual == "/images/silla-de-cine.png") {
+                            img.setSrc("/images/silla-de-cine-clicada.png");
+                        }
+
+                        if (actual == "/images/silla-de-cine-clicada.png") {
+                            img.setSrc("/images/silla-de-cine.png");
+                            cont_asientos--;
+                            num_elegida--;
+                            img.setAlt("0");
+                            }
+
+                        });
+                    enviar.addClickListener(e -> {
+                        UI.getCurrent().getSession().setAttribute("clicks", num_clicks);
+                        UI.getCurrent().getSession().setAttribute("num_asientos", cont_asientos);
+                        UI.getCurrent().getSession().setAttribute("filcol",img);
+                        UI.getCurrent().navigate(compraview.class);
+                    });
+
+                    h.setSpacing(false);
+                    h.setWidthFull();
+                    h.setJustifyContentMode(JustifyContentMode.CENTER);
+                    h.add(img);
+                    add(h,enviar);
+                }
             }
+        }
+
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        if(UI.getCurrent().getSession().getAttribute("filas") == null){
+            event.forwardTo(ImageListView.class);
         }
     }
 }
