1fe3c48e8ec18367f8309aa3242d98071aeaeea7
Berto-e <65825564+Berto-e@users.noreply.github.com>
2022-01-13 01:44:23 +0100

    MailSenderService

--- src/main/java/com/example/application/repositories/EmailSenderService.java
+++ src/main/java/com/example/application/repositories/EmailSenderService.java
@@ -1,10 +1,14 @@
 package com.example.application.repositories;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.Properties;
 
 @Service
 public class EmailSenderService{
@@ -20,6 +24,24 @@ public class EmailSenderService{
         message.setSubject(subject);
         message.setText(text);
         emailSender.send(message);
+    }
+
+    @Bean
+    public JavaMailSender getJavamailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("discovercinemaservice@gmail.com");
+        mailSender.setPassword("hsdlnmqlffzlwkhf");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
 
 }
